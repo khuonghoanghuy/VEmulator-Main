4e1e86de505acb7dbae2c9b56f204445ca055c2d
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-07-21 12:28:15 +0700

    code to much

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace VEmualtor
             Console.WriteLine("Please using command like 'help' for more info about this app");
 
             string input = Console.ReadLine();
+            bool atQEMU = false;
             
             switch (input)
             {
@@ -18,6 +19,29 @@ namespace VEmualtor
                     Console.WriteLine("- HELP -");
                     Console.WriteLine("Command List: \n- `start` start run qemu with command\n- `exit` exit app");
                     return;
+
+                case "start":
+                    Console.Clear();
+                    atQEMU = true;
+                    if (atQEMU)
+                    {
+                        Console.WriteLine("Set your RAM");
+                        string ram = Console.ReadLine();
+                        Console.Write("Your ram is: " + ram);
+
+                        Console.WriteLine("Set your cpu");
+                        string cpu = Console.ReadLine();
+                        Console.Write("Your cpu is: " + cpu);
+
+                        Console.WriteLine("Path your Hard disk");
+                        string hd = Console.ReadLine();
+                        Console.WriteLine("Your hard disk from: " + hd);
+
+                        string code = "-m " + ram + " -hda " + hd + " -cpu " + cpu;
+
+                        System.Diagnostics.Process.Start("qemu-system-x86_64.exe", code);
+                    }
+                    break;
 
                 case "exit":
                     System.Environment.Exit(1);
